370b146324d11d153950cc300156a7ada4cb50fd
shkim1224 <shkim@kunsan.ac.kr>
2023-04-10 22:21:35 -0700

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,17 @@
+import turtle
+import pickle
+
+t = turtle.Turtle()
+t.shape("turtle")
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+
+turtle.mainloop()
+turtle.bye()
